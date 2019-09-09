822a1fd87721587c942aa32944753eb566bf4894
ualexander <aleanches@gmail.com>
2019-09-10 00:20:11 +0300

    update generator sh

--- generator/make_test.sh
+++ generator/make_test.sh
@@ -1,34 +1,22 @@
 #!/bin/bash
 
-param=$1
-
 chmod 777 generator.dms
 
-if [ $param == 1 ]
+if [ $1 == 1 ]
 then
-	./generator.dms --flow-one > param_1_flow_one
-	time ../lem-in < param_1_flow_one | grep "L" | wc -l
-
-elif [ $param == 2 ]
+	map="flow-one"
+elif [ $1 == 2 ]
 then
-	./generator.dms --flow-ten > param_2_flow_ten
-	time ../lem-in < param_2_flow_ten | grep "L" | wc -l
-
-elif [ $param == 3 ]
+	map="flow-ten"
+elif [ $1 == 3 ]
 then
-	./generator.dms --flow-thousand > param_3_flow_thousand
-	time ../lem-in < param_3_flow_thousand | grep "L" | wc -l
-
-elif [ $param == 4 ]
+	map="flow-thousand"
+elif [ $1 == 4 ]
 then
-	./generator.dms --big > param_4_big
-	time ../lem-in < param_4_big | grep "L" | wc -l
-
-elif [ $param == 5 ]
+	map="big"
+elif [ $1 == 5 ]
 then
-	./generator.dms --big-superposition > param_5_big_superposition
-	time ../lem-in < param_5_big_superposition | grep "L" | wc -l
-
+	map="big-superposition"
 else
 	echo "use param:"
 	echo "for flow_one - 1"
@@ -36,4 +24,18 @@ else
 	echo "for flow_thousand - 3"
 	echo "for big - 4"
 	echo "for big_superposition - 5"
+	exit
+fi
+
+echo "\nmap: $map"
+./generator.dms --$map > $map
+time your=$(../lem-in < $map | grep "L" | wc -l | awk '{ gsub (" ", "", $0); print}')
+required=$(cat $map | grep "#Here is the number of lines required:" |
+head -1 | awk '{ gsub ("#Here is the number of lines required: ", "", $0); print}')
+
+if (("$your" <= "$required"))
+then
+	echo "\n✅  your result = $your | required result = $required\n"
+else
+	echo "\n⭕️  your result = $your | required result = $required | difference = $(($your - $required))\n"
 fi
